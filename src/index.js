ab35fc368076718cb68dcf8ada93b4ea3398744b
78-artilleryman <ywy040150@gmail.com>
2024-02-11 22:00:02 +0900

    project upload

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { LocaleProvider } from './contexts/LocaleContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <LocaleProvider defaultValue="ko">
+  <App />
+</LocaleProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
